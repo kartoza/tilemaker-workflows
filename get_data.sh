f665ee5d37dbddb58fd237fc6fde8ecec19d716f
Tim Sutton <tim@kartoza.com>
2024-02-05 13:12:46 +0000

    get data update

--- get_data.sh
+++ get_data.sh
@@ -11,11 +11,44 @@ else
   cd ../../
 fi
 
+if test -f "landcover//.shp"; then
+  echo "file exists."
+else
+  echo "does not exist."
+  mkdir -p landcover/
+  cd landcover/
+  curl -OL 
+  unzip .zip
+  rm .zip
+  cd ../../
+fi
+
+if test -f "landcover//.shp"; then
+  echo "file exists."
+else
+  echo "does not exist."
+  mkdir -p landcover/
+  cd landcover/
+  curl -OL 
+  unzip .zip
+  rm .zip
+  cd ../../
+fi
+
+if test -f "coastline/water-polygons-split-4326.shp"; then
+  echo "coastline/water-polygons-split-4326.shp exists."
+else
+  echo "coastline/water-polygons-split-4326.shp does not exist."
+  mkdir -p coastline/
+  cd coastline/
+  curl -OL https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip
+  unzip water-polygons-split-4326.zip
+  rm water-polygons-split-4326.zip
+  cd ../
+fi
 
 #curl -OL https://naciscdn.org/naturalearth/10m/physical/ne_10m_antarctic_ice_shelves_polys.zip
 #curl -OL https://naciscdn.org/naturalearth/10m/physical/ne_10m_glaciated_areas.zip
-#curl -OL https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf
-#curl -OL https://osmdata.openstreetmap.de/download/water-polygons-split-4326.zip
 #mkdir ne_10m_antarctic_ice_shelves_polys
 #mkdir ne_10m_glaciated_areas
 #cd ../ne_10m_glaciated_areas/
